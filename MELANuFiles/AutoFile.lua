bcafde3ac77220a2c9049efb8c64949b8782cec5
D-EV-ME-LA-NO <88006057+D-EV-ME-LA-NO@users.noreply.github.com>
2023-01-30 11:47:17 +0300

    Update AutoFile.lua

--- MELANuFiles/AutoFile.lua
+++ MELANuFiles/AutoFile.lua
@@ -21,14 +21,14 @@ end
 
 
 
-if text == '᥀›تفعيل النسخه التلقائيه' then   
+if text == '᥀ ›تفعيل النسخه التلقائيه' then   
 if not msg.Asasy then 
 return send(msg_chat_id,msg_id,'\n*⌔︙هاذا الامر يخص { '..Controller_Num(1)..' }* ',"md",true)  
 end
 Redis:setex(TheMalak.."Status:SendFile",28800,true) 
 return send(msg_chat_id,msg_id,"⌔︙تم تفعيل جلب نسخة الكروبات التلقائيه","md")
 end
-if text == '᥀›تعطيل النسخه التلقائيه' then  
+if text == '᥀ ›تعطيل النسخه التلقائيه' then  
 if not msg.Asasy then 
 return send(msg_chat_id,msg_id,'\n*⌔︙هاذا الامر يخص { '..Controller_Num(1)..' }* ',"md",true)  
 end
